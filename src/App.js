7f41e0ebee556159d4062c189d9bf60c8aa1f8d1
IXISunnyIXI <alisi125401@gmail.com>
2021-03-13 00:49:16 -0500

    Initial Commit
    Started creating React based frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const title = 'Welcome to the Product Checker';
+  const link = "https://www.reddit.com"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />                      {/*same as <Navbar></Navbar>*/}
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
 
+{/*always export component function so it can be used in other files - index.js*/}
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>Product Checker</h1>
+            <div className="links">
+                <a href="/">Home</a><br/>
+                <a href="/create">Add Product</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
