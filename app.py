cc2ac117e783032ef1d2349113e2a50cd94a22c7
IXISunnyIXI <alisi125401@gmail.com>
2021-03-23 01:17:14 -0400

    Creating Flask App
    Complete start from scratch using Flask instead of React and backend language.

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+        return render_template("home.html")
+
+@app.route("/login")
+def about():
+        return render_template("login.html")
+
+if __name__ == '__main__':
+    app.run(debug==True)
