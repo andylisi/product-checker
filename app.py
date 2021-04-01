0dc6ca0b11115985820b6cb2bcd9c5f02ddb0cd9
IXISunnyIXI <alisi125401@gmail.com>
2021-04-01 02:03:44 -0400

    SQLAlchemy and UI Tweaks
    Configured SQLAlchemy and setup some test models. Models will still need to be updated to be considered complete. Also currently using SQLlite and will switch to MYSQL.
    
    Changed some UI elements like navbar and button color themes.

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '2rQ5uFcZHFB5iEvw4ZVYgSFYgozDUjA9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+# This class will create a table schema within the db.
+# Set datatypes, uqique, req/nullable and keys
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    products = db.relationship('Product', backref='user', lazy=True)
+
+    #__repr__ is used to compute the “official” string representation of an object 
+    # and is typically used for debugging.
+    def __repr__(self):
+        return f"User('{self.password}', '{self.username}', '{self.email}', '{self.image_file}')"
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(30), unique=True, nullable=False)
+    retailer = db.Column(db.String(30), nullable=False)
+    url = db.Column(db.String(150), unique=True, nullable=False)
+    date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Product('{self.name}', '{self.retailer}', '{self.date_added}')"
 
 products = [
     {
